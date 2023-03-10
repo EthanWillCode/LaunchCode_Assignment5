2e86abce5a238a8d17cec21d3e23a59a18f78c9a
EthanWillCode <Aethil@yahoo.com>
2023-03-09 21:26:43 -0600

    Updated Shuttle status lines to improve user experiance. Added required pop-up windows for invalid entries.

--- scriptHelper.js
+++ scriptHelper.js
@@ -51,59 +51,63 @@ function formSubmission(document, pilot, copilot, fuelLevel, cargoMass) {
     //vPilot handler
         if(vPilot === "Not a Number"){
             allStatus[0] = true;
-            pilotStatus.innerHTML =`${pilot} ready!`
+            pilotStatus.innerHTML =`1. Pilot ${pilot} is ready for launch!`
         } else if(vPilot === "empty") {
             allStatus[0] = false;
-            pilotStatus.innerHTML = "Enter a name"
+            pilotStatus.innerHTML = "1. Please enter a name"
         } else {
             allStatus[0] = false;
-            pilotStatus.innerHTML =`${pilot} is an invalid entry!`
+            pilotStatus.innerHTML =`1. ${pilot} is an invalid entry!`
+            alert("Please enter a name!")
         }
     //vcoPilot handler
         if(vCopilot === "Not a Number"){
             allStatus[1] = true;
-            copilotStatus.innerHTML =`${copilot} ready!`
+            copilotStatus.innerHTML =`2. Copilot ${copilot} is ready for Launch!`
         } else if(vCopilot === "empty") {
             allStatus[1] = false;
-            copilotStatus.innerHTML = "Enter a name"
+            copilotStatus.innerHTML = "2. Please enter a name."
         } else {
             allStatus[1] = false;
-            copilotStatus.innerHTML =`${copilot} is an invalid entry!`
+            copilotStatus.innerHTML =`2. ${copilot} is an invalid entry!`
+            alert("Please enter a name!")
         }
     //vFuelLevel handler
     console.log(fuelLevel)
     console.log(vFuelLevel)
         if(vFuelLevel === "Is a Number" && fuelLevel >= 10000){
             allStatus[2] = true;
-            fuelStatus.innerHTML = "Fuel level high enough for launch"
+            fuelStatus.innerHTML = "3. Fuel level high enough for launch"
         } else if(vFuelLevel === "empty") {
             allStatus[2] = false;
-            fuelStatus.innerHTML = "Enter the Fuel Level"
+            fuelStatus.innerHTML = "3. Enter the Fuel Level"
         }else if (vFuelLevel === "Not a Number"){
             allStatus[2] = false;
-            fuelStatus.innerHTML = "Invalid Entry"
+            fuelStatus.innerHTML = `3. ${fuelLevel} is not a valid entry`
+            alert("Please enter a number!")
         } else {
             allStatus[2] = false;
-            fuelStatus.innerHTML = "Fuel level too low for launch"
+            fuelStatus.innerHTML = "3. Fuel level too low for launch"
         }
     //vCargoMass handler
         console.log(vCargoMass)
         if(vCargoMass === "Is a Number" && cargoMass <= 10000){
             allStatus[3] = true;
-            cargoStatus.innerHTML = "Cargo mass low enough for launch"
+            cargoStatus.innerHTML = "4. Cargo mass low enough for launch"
         } else if(vCargoMass === "empty") {
             allStatus[3] = false;
-            cargoStatus.innerHTML = "Enter cargo mass"
+            cargoStatus.innerHTML = "4. Enter cargo mass"
         } else if(vCargoMass === "Not a Number"){
             allStatus[3] = false;
-            cargoStatus.innerHTML = "Invalid Entry"
+            cargoStatus.innerHTML = `3. ${cargoMass} is not a valid entry`
+            alert("Please enter a number!")
         } else {
             allStatus[3] = false;
-            cargoStatus.innerHTML = "Cargo mass too heavy for launch"
+            cargoStatus.innerHTML = "4. Cargo mass too heavy for launch"
         }
     //launchStatus handler
     console.log(allStatus)
-        if(allStatus[0] === true,allStatus[1] === true,allStatus[2] === true,allStatus[3] === true){
+        if(allStatus[0] === true && allStatus[1] === true && allStatus[2] === true && allStatus[3] === true){
             launchStatus.innerHTML = "Shuttle is Ready for Launch"
             launchStatus.style.color = "green"
         } else if(allStatus.includes(false)){
